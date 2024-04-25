2fcaf27533c9217cb0f9f3b8606389f1c2f07864
bwbhub <brice.bouhot@gmail.com>
2024-04-25 11:41:50 +0200

    first commit

---
+++ client/src/App.tsx
@@ -0,0 +1,5 @@
+const App = (): JSX.Element => {
+  return <main>Hello World</main>;
+};
+
+export default App;
---
+++ client/src/assets/index.ts
@@ -0,0 +1,9 @@
+const assets: {} = {
+  images: {
+    logoDark: require("./images/logo-dark.png"),
+    logoight: require("./images/logo-light.png"),
+    appIcon: require("favicon.png"),
+  },
+};
+
+export default assets;
